d799ae48e64bf006258b0bda9c6da9170b4f2024
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2024-03-07 15:44:54 -0300

    claroOscuroCompletado

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const abrirMenu = document.querySelector(".fa-bars");
 const cerrarMenu = document.querySelector(".fa-xmark");
 //  Menú (Hamburguesa)
 const barsMenu = document.querySelector(".navbar-list");
-//  Overlay para tirar facha abajo del menú hamburguesa y el cart.
+//  Overlay para tirar facha abajo del menú hamburguesa 
 const overlay = document.querySelector(".overlay");
 // Logo menu responsive antes de abrirlo
 const logoResponsive = document.querySelector(".fa-bars");
@@ -125,11 +125,354 @@ const actualizarIconoClaroOscuro = () => {
   if (botonLuna.classList.contains("fa-moon-active")) {
     botonLuna.classList.remove("fa-moon-active");
     botonSol.classList.add("fa-sun-active");
+
   } else if (botonSol.classList.contains("fa-sun-active")) {
     botonSol.classList.remove("fa-sun-active");
     botonLuna.classList.add("fa-moon-active");
+
+
   }
 };
+
+//Funcion para cambiar estilos Claro/Oscuro
+
+const cssClaroOscuro = () => {
+  let modoClaro = false;
+  // Cambia el estado del modo claro/oscuro
+  if (botonSol.classList.contains("fa-sun-active")) {
+    modoClaro = !modoClaro;
+  }
+
+  if (modoClaro) {
+    // Capturar elementos a modificar color
+    let gaby = document.querySelector(".gaby");
+    let enlaces = document.querySelectorAll(".navbar-link");
+    let colorDeFondo = document.body;
+    let spanHola = document.querySelector(".spanTitulo");
+    let spanEstudiante = document.querySelector(".spanTitulo2");
+    let gitHub = document.querySelector(".fa-github");
+    let linkedIn = document.querySelector(".fa-linkedin");
+    let contacto = document.querySelector(".fa-square-phone");
+    let divDecoration = document.querySelector(".divDecoration");
+    let fontsNegritas = document.querySelectorAll(".fontNegrita");
+    let spansTexts = document.querySelectorAll(".spanText");
+    let pSobreMi = document.querySelector(".pSobreMi");
+    let h3Educacion = document.querySelector(".h3Educacion");
+    let logosCalendarios = document.querySelectorAll(".fa-calendar-check");
+    let pEducacionStrong = document.querySelectorAll(".pClaroOscuro");
+    let spansEducacion = document.querySelectorAll(".spanClaroOscuro");
+    let texts4x1 = document.querySelectorAll(".text4x1");
+    let logosGitHubProyectos = document.querySelectorAll(".githubproyect");
+    let logosGitHubOtrosProyectos = document.querySelectorAll(".githubotrosProyectos");
+    let logosVercelProyectos = document.querySelectorAll(".fa-globe");
+    let logosVercelOtrosProyectos = document.querySelectorAll(".fa-globeProyectos");
+    let h3OtrosProyectos = document.querySelectorAll(".h3OtroProyecto");
+    let pOtrosProyectos = document.querySelectorAll(".pCardOtrosProyectos");
+    let h1Contacto = document.querySelector(".h1Contacto");
+    let pContacto = document.querySelector(".pContacto");
+    let logoEmailContacto = document.querySelector(".fa-envelope");
+    let logoTelefonoContacto = document.querySelector(".fa-sq-ph2");
+    let emailTelefonoContacto = document.querySelectorAll(".font-bold");
+    let inputsContacto = document.querySelectorAll(".contacto-datos");
+    let h2Footer = document.querySelector(".h2Footer");
+    let pFooter = document.querySelector(".pFooter");
+    let iconosFooter = document.querySelectorAll(".iconoFooter");
+
+
+
+    // Modificar colores
+    gaby.style.color = "#1f2023";
+
+    enlaces.forEach(enlace => {
+      enlace.style.color = "#1f2023";
+    });
+
+    colorDeFondo.style.backgroundColor = "#fff";
+
+    spanHola.style.color = "#1f2023";
+
+    spanEstudiante.style.color = "#1f2023";
+
+    gitHub.style.color = "#636363"
+
+    linkedIn.style.color = "#636363";
+
+    contacto.style.color = "#636363";
+
+    divDecoration.style.borderColor = "#636363";
+
+    fontsNegritas.forEach(enlace => {
+      enlace.style.color = "#000";
+    });
+
+    spansTexts.forEach(enlace => {
+      enlace.style.color = "#1f2023";
+    });
+
+    pSobreMi.style.color = "#1f2023";
+
+    h3Educacion.style.color = "#000";
+
+    logosCalendarios.forEach(enlace => {
+      enlace.style.color = "#1f2023";
+    });
+
+    pEducacionStrong.forEach(enlace => {
+      enlace.style.color = "#000";
+    });
+
+    spansEducacion.forEach(enlace => {
+      enlace.style.color = "#1f2023";
+    });
+
+    texts4x1.forEach(enlace => {
+      enlace.style.color = "#000";
+    });
+
+    logosGitHubProyectos.forEach(enlace => {
+      enlace.style.color = "#000";
+    });
+
+    //Hover para enlaces 
+    logosGitHubProyectos.forEach(elemento => {
+      // Agrega el efecto al pasar el mouse sobre el elemento
+      elemento.addEventListener('mouseover', () => {
+        elemento.style.color = "#5f4983";
+      });
+      // Elimina el efecto al sacar el mouse del elemento
+      elemento.addEventListener('mouseout', () => {
+        elemento.style.color = '#000';
+      });
+    });
+
+    logosVercelProyectos.forEach(enlace => {
+      enlace.style.color = "#000";
+    });
+
+    logosVercelProyectos.forEach(elemento => {
+      // Agrega el efecto al pasar el mouse sobre el elemento
+      elemento.addEventListener('mouseover', () => {
+        elemento.style.color = "#5f4983";
+      });
+      // Elimina el efecto al sacar el mouse del elemento
+      elemento.addEventListener('mouseout', () => {
+        elemento.style.color = '#000';
+      });
+    });
+
+    logosGitHubOtrosProyectos.forEach(enlace => {
+      enlace.style.color = "#636363";
+    });
+
+    logosGitHubOtrosProyectos.forEach(elemento => {
+      // Agrega el efecto al pasar el mouse sobre el elemento
+      elemento.addEventListener('mouseover', () => {
+        elemento.style.color = "#5f4983";
+      });
+      // Elimina el efecto al sacar el mouse del elemento
+      elemento.addEventListener('mouseout', () => {
+        elemento.style.color = '#636363';
+      });
+    });
+
+    logosVercelOtrosProyectos.forEach(enlace => {
+      enlace.style.color = "#636363";
+    });
+
+    logosVercelOtrosProyectos.forEach(elemento => {
+      // Agrega el efecto al pasar el mouse sobre el elemento
+      elemento.addEventListener('mouseover', () => {
+        elemento.style.color = "#5f4983";
+      });
+      // Elimina el efecto al sacar el mouse del elemento
+      elemento.addEventListener('mouseout', () => {
+        elemento.style.color = '#636363';
+      });
+    });
+
+    h3OtrosProyectos.forEach(enlace => {
+      enlace.style.color = "#000";
+    });
+
+    pOtrosProyectos.forEach(enlace => {
+      enlace.style.color = "#1f2023";
+    });
+
+    h1Contacto.style.color = "#1f2023";
+
+    pContacto.style.color = "#1f2023";
+
+    logoEmailContacto.style.color = "#000";
+
+    logoTelefonoContacto.style.color = "#000";
+
+    emailTelefonoContacto.forEach(elemento => {
+      elemento.style.color = "#000";
+    })
+
+    inputsContacto.forEach(elemento => {
+      elemento.style.backgroundColor = "#fff";
+    })
+
+    h2Footer.style.color = "#000";
+
+    pFooter.style.color = "#000";
+
+    iconosFooter.forEach(elemento =>{
+      elemento.style.color="#636363";
+    })
+
+     //Hover para iconos del footer
+     iconosFooter.forEach(elemento => {
+      // Agrega el efecto al pasar el mouse sobre el elemento
+      elemento.addEventListener('mouseover', () => {
+        elemento.style.color = "#5f4983";
+      });
+      // Elimina el efecto al sacar el mouse del elemento
+      elemento.addEventListener('mouseout', () => {
+        elemento.style.color = '#636363';
+      });
+    });
+
+  } else {
+    // Elimina los estilos del modo claro y restaura los estilos oscuros por defecto
+    // Capturar elementos a modificar color
+    let gaby = document.querySelector(".gaby");
+    let enlaces = document.querySelectorAll(".navbar-link");
+    let colorDeFondo = document.body;
+    let spanHola = document.querySelector(".spanTitulo");
+    let spanEstudiante = document.querySelector(".spanTitulo2");
+    let gitHub = document.querySelector(".fa-github");
+    let linkedIn = document.querySelector(".fa-linkedin");
+    let contacto = document.querySelector(".fa-square-phone");
+    let divDecoration = document.querySelector(".divDecoration");
+    let fontsNegritas = document.querySelectorAll(".fontNegrita");
+    let spansTexts = document.querySelectorAll(".spanText");
+    let pSobreMi = document.querySelector(".pSobreMi");
+    let h3Educacion = document.querySelector(".h3Educacion");
+    let logosCalendarios = document.querySelectorAll(".fa-calendar-check");
+    let pEducacionStrong = document.querySelectorAll(".pClaroOscuro");
+    let spansEducacion = document.querySelectorAll(".spanClaroOscuro");
+    let texts4x1 = document.querySelectorAll(".text4x1");
+    let logosGitHubProyectos = document.querySelectorAll(".githubproyect");
+    let logosGitHubOtrosProyectos = document.querySelectorAll(".githubotrosProyectos");
+    let logosVercelProyectos = document.querySelectorAll(".fa-globe");
+    let logosVercelOtrosProyectos = document.querySelectorAll(".fa-globeProyectos");
+    let h3OtrosProyectos = document.querySelectorAll(".h3OtroProyecto");
+    let pOtrosProyectos = document.querySelectorAll(".pCardOtrosProyectos");
+    let h1Contacto = document.querySelector(".h1Contacto");
+    let pContacto = document.querySelector(".pContacto");
+    let logoEmailContacto = document.querySelector(".fa-envelope");
+    let logoTelefonoContacto = document.querySelector(".fa-sq-ph2");
+    let emailTelefonoContacto = document.querySelectorAll(".font-bold");
+    let inputsContacto = document.querySelectorAll(".contacto-datos");
+    let h2Footer = document.querySelector(".h2Footer");
+    let pFooter = document.querySelector(".pFooter");
+    let iconosFooter = document.querySelectorAll(".iconoFooter");
+
+
+
+    gaby.style.color = "";
+
+    enlaces.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    colorDeFondo.style.backgroundColor = "";
+
+    spanHola.style.color = "";
+
+    spanEstudiante.style.color = "";
+
+    gitHub.style.color = "";
+
+    linkedIn.style.color = "";
+
+    contacto.style.color = "";
+
+    divDecoration.style.borderColor = "";
+
+    fontsNegritas.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    spansTexts.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    pSobreMi.style.color = "";
+
+    h3Educacion.style.color = "";
+
+    logosCalendarios.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    pEducacionStrong.forEach(enlace => {
+      enlace.style.color = "";
+    });
+    spansEducacion.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    texts4x1.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    logosGitHubProyectos.forEach(enlace => {
+      enlace.style.color = "";
+    });
+    
+    logosVercelProyectos.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    logosGitHubOtrosProyectos.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    logosVercelOtrosProyectos.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    h3OtrosProyectos.forEach(enlace => {
+      enlace.style.color = "";
+    });
+
+    pOtrosProyectos.forEach(enlace => {
+      enlace.style.color = "";
+    });
+  
+  
+    h1Contacto.style.color = "";
+
+    pContacto.style.color = "";
+
+    logoEmailContacto.style.color = "";
+
+    logoTelefonoContacto.style.color = "";
+
+    emailTelefonoContacto.forEach(elemento => {
+      elemento.style.color = "";
+    })
+
+    inputsContacto.forEach(elemento => {
+      elemento.style.backgroundColor = "";
+    })
+
+    h2Footer.style.color = "";
+
+    pFooter.style.color = "";
+
+    iconosFooter.forEach(elemento =>{
+      elemento.style.color="";
+    })
+
+  }
+
+ 
+
+}
 
 // Puerta de entrada
 const init = () => {
@@ -141,5 +484,6 @@ const init = () => {
   window.addEventListener("scroll", irArriba);
   formulario.addEventListener("submit", controlarSubmit);
   divClaroOscuro.addEventListener("click", actualizarIconoClaroOscuro);
+  divClaroOscuro.addEventListener("click", cssClaroOscuro);
 };
 init();
